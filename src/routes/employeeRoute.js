863bc315f90889efaaa4783d7d2b4756b7357c90
tusharpethani1 <tushar.pethani@miraitech.com.au>
2024-03-20 14:17:28 +0530

    fixed deployment issue-2

--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -1,31 +1,31 @@
-import express from "express";
-import multer from "multer";
+import express from 'express';
+import multer from 'multer';
 import {
-  signUp,
-  login,
-  updateEmployee,
-  saveEmployee,
-  uploadProfilePicture,
-  getProfileDetailsById,
-  getUser,
-  getLoginUser,
-  uploadNewResume,
-  sendOTP,
-} from "../controllers/employee/employeeController.js";
+    signUp,
+    login,
+    updateEmployee,
+    saveEmployee,
+    uploadProfilePicture,
+    getProfileDetailsById,
+    getUser,
+    getLoginUser,
+    uploadNewResume,
+    sendOTP,
+} from '../controllers/employee/employeeController.js';
 
-const upload = multer({ dest: "uploads/" });
+const upload = multer({ dest: 'temp/' });
 
 const router = express.Router();
 
-router.post("/signup", signUp);
-router.post("/login", login);
-router.put("/upload/profile/:id", upload.single("image"), uploadProfilePicture);
-router.put("/upload/resume/:id", upload.single("resume"), uploadNewResume);
-router.post("/save", saveEmployee);
-router.get("/user/:id", getUser);
-router.put("/update/:id", updateEmployee);
-router.get("/users/me", getLoginUser);
-router.get("/getdetails/:id", getProfileDetailsById);
-router.post("/otpgenerate", sendOTP);
+router.post('/signup', signUp);
+router.post('/login', login);
+router.put('/upload/profile/:id', upload.single('image'), uploadProfilePicture);
+router.put('/upload/resume/:id', upload.single('resume'), uploadNewResume);
+router.post('/save', saveEmployee);
+router.get('/user/:id', getUser);
+router.put('/update/:id', updateEmployee);
+router.get('/users/me', getLoginUser);
+router.get('/getdetails/:id', getProfileDetailsById);
+router.post('/otpgenerate', sendOTP);
 
-export  { router };
+export { router };
--- src/routes/keySkillsRoutes.js
+++ src/routes/keySkillsRoutes.js
@@ -1,74 +1,76 @@
-import express from 'express';
 import { Router } from 'express';
-import fs from "fs";
-import { promisify } from "util";
+import fs from 'fs';
+import { promisify } from 'util';
 import KeySkill from '../models/skillSetModal.js';
-import { moduleNames } from '../utils/enums.js';
-import { successResponse } from '../utils/helpers.js';
+import { errorResponse, successResponse } from '../utils/helpers.js';
 
 const router = Router();
 
 const readFileAsync = promisify(fs.readFile);
 
 router.post('/', async (req, res) => {
-  // Expecting req.body to be an array of key skills
-  const skills = await readFileAsync("dataFormat.json", "utf-8");
+    // Expecting req.body to be an array of key skills
+    const skills = await readFileAsync('dataFormat.json', 'utf-8');
     const parsedSkills = JSON.parse(skills);
-  const keySkills = parsedSkills;
+    const keySkills = parsedSkills;
 
-  try {
-    // Validate input to ensure it's an array
-    if (!Array.isArray(keySkills)) {
-      logger.error(
-        `${moduleNames[0]} - Post-Status:${400}, Error:${JSON.stringify(
-          "Input should be an array of key skills"
-        )}`
-      );
-      return errorResponse(res, 400, "Input should be an array of key skills.")
+    try {
+        // Validate input to ensure it's an array
+        if (!Array.isArray(keySkills)) {
+            // logger.error(
+            //   `${moduleNames[0]} - Post-Status:${400}, Error:${JSON.stringify(
+            //     "Input should be an array of key skills"
+            //   )}`
+            // );
+            return errorResponse(
+                res,
+                400,
+                'Input should be an array of key skills.'
+            );
+        }
+
+        // Optional: Add validation here to check each object in the array
+        // for the expected structure ({ label: 'string', value: 'string' })
+
+        const inserted = await KeySkill.insertMany(keySkills);
+        // logger.info(`${moduleNames[0]} - Get. Success:${JSON.stringify(inserted)}`);
+        return successResponse(res, 200, '', inserted);
+    } catch (error) {
+        // logger.error(
+        //     `${moduleNames[0]} - Get-Status:${500}, Error:${JSON.stringify(
+        //       "Employee not found"
+        //     )}`
+        //   );
+        return errorResponse(res, 400, error.message);
     }
-
-    // Optional: Add validation here to check each object in the array
-    // for the expected structure ({ label: 'string', value: 'string' })
-
-    const inserted = await KeySkill.insertMany(keySkills);
-    logger.info(`${moduleNames[0]} - Get. Success:${JSON.stringify(inserted)}`);
-    successResponse(res, 200, "",inserted);
-  } catch (error) {
-    logger.error(
-        `${moduleNames[0]} - Get-Status:${500}, Error:${JSON.stringify(
-          "Employee not found"
-        )}`
-      );
-      return errorResponse(res, 400, error.message);
-  }
 });
 
 router.get('/', async (req, res) => {
-  const searchTerm = req.query.searchTerm;
+    const { searchTerm } = req.query;
 
-  try {
-    if (!searchTerm) {
-      logger.error(
-        `Get-Status:${400}, Error:${JSON.stringify(
-          "A search term is required"
-        )}`
-      );
-      return  errorResponse(res, 400, "A search term is required.");
+    try {
+        if (!searchTerm) {
+            // logger.error(
+            //   `Get-Status:${400}, Error:${JSON.stringify(
+            //     "A search term is required"
+            //   )}`
+            // );
+            return errorResponse(res, 400, 'A search term is required.');
+        }
+
+        // Perform a case-insensitive search and limit results to 10
+        const skills = await KeySkill.find({
+            label: { $regex: new RegExp(searchTerm, 'i') },
+        }).limit(10);
+        return successResponse(res, 200, '', skills);
+    } catch (error) {
+        // logger.error(
+        //   `${moduleNames[0]} - Get-Status:${500}, Error:${JSON.stringify(
+        //     error.message
+        //   )}`
+        // );
+        return errorResponse(res, 500, error.message);
     }
-
-    // Perform a case-insensitive search and limit results to 10
-    const skills = await KeySkill.find({ 
-      label: { $regex: new RegExp(searchTerm, 'i') } 
-    }).limit(10);
-    successResponse(res, 200, "",skills);
-  } catch (error) {
-    logger.error(
-      `${moduleNames[0]} - Get-Status:${500}, Error:${JSON.stringify(
-        error.message
-      )}`
-    );
-     errorResponse(res,500, error.message);
-  }
 });
 
 export default router;
